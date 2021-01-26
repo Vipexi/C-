1080c0902a198359a10c42244b31924aee79c3a8
vipexi <ville.vihtalahti@student.samk.fi>
2021-01-26 11:54:19 +0200

    C#

---
+++ Conditions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+            if (num == 0) {
+                Console.WriteLine("true");
+            }
+
+            //elif
+            else if (num ==2) {
+                Console.WriteLine("2");
+            }
+
+            else {
+                Console.WriteLine("false");
+            }
+
+            if (num != 4) {
+                Console.WriteLine("not four");
+            }
+
+            /*bool a = false;
+            bool b = true;
+
+            // and &&
+            if (a == true && b == true) {
+                Console.WriteLine("Both are True!");
+
+            }
+            // or ||
+            if (a == true || b == true) {
+                Console.WriteLine("The other one is true!");
+            }*/
+
+
+        }
+    }
+}
---
+++ SwitchCase/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int num = 10;
+
+            switch(num) {
+
+                case 1:
+                    Console.WriteLine(1);
+                    break;
+
+                case 2:
+                    Console.WriteLine(2);
+                    break;
+
+                case 3:
+                    Console.WriteLine("3 or 4");
+                    break;
+
+                default:
+                    Console.WriteLine("something");
+                    break;
+
+            }
+            /*int caseSwitch= 1;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                
+                default:
+                    Console.WriteLine("Neither!");
+                    break;
+                
+            }*/
+        }
+    }
+}
